2d5c87a97e1f44387484c95e498100d8d01ca2a3
CrispyBrownies <stephenc1999@gmail.com>
2022-05-06 20:12:34 -0400

    Add tamagotchi.cpp and pet.cpp.

---
+++ src/pet.cpp
@@ -0,0 +1,31 @@
+// Stephen Chen
+// Created: 5/5/2022
+// Main file for handle pet behaviors
+
+#include <string>
+
+using namespace std
+
+class Pet {
+	private:
+		int id;
+		string name;
+		int age;
+		int health;
+		int hunger;
+		int happiness;
+		int sickness;
+		bool sick;
+
+		int ageRate;
+		int healthRate;
+		int hungerRate;
+		int happinessRate;
+		int sicknessRate;
+
+	public:
+
+
+
+
+};
---
+++ src/tamagotchi.cpp
@@ -0,0 +1,11 @@
+// Stephen Chen
+// Created: 5/5/2022
+// Main file for the Tamagotchi game
+
+int main() {
+
+}
+
+
+
+
